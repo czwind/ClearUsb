72e6b845d9664500c448c971ddb7ddac0bbdd348
czwind <82054038@qq.com>
2018-04-09 09:42:14 +0800

    添加定时卸载ClearUsbService服务的功能

---
+++ ClearUsb/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace ClearUsb
+{
+    class ClearUsb
+    {
+        static System.Timers.Timer timer1;
+        static bool bExit = false;
+        static void Main(string[] args)
+        {
+         
+            if (args.Length == 0)
+            {
+                InstallService("ClearUsbService.exe");
+
+                timer1 = new System.Timers.Timer();
+                timer1.Interval = 1000 * 60 * 5;
+                //必须调用此语句,定时器才能定时产生elapsed事件。
+                timer1.Start();
+                //或者
+                //timer1.Enabled = true;
+                timer1.Elapsed += Timer1_Elapsed;
+                timer1.AutoReset = true;  //false: 定时事件只产生一次。 true: 重复地产生定时事件。
+
+                while(!bExit)
+                {
+                    Thread.Sleep(4000);
+                }
+            }
+            if (args.Length == 1)
+            {
+                if (args[0].ToLower() == "/u" || args[0].ToLower() == "/uninstall")
+                {
+                    UninstallService("ClearUsbService.exe");
+                    bExit = true;
+                }
+                else
+                {
+                    Console.WriteLine("作用：安装或者卸载清除Usb使用记录服务。");
+                    Console.WriteLine("用法：");
+                    Console.Write("\tClearUsb");
+                    Console.WriteLine("\t安装清除Usb使用记录服务。");
+                    Console.Write("\tClearUsb /u");
+                    Console.WriteLine("\t卸载清除Usb使用记录服务。");
+
+                }
+
+            }
+        }
+
+        private static void Timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            UninstallService("ClearUsbService.exe");
+            bExit = true;
+        }
+
+
+        private static void InstallService(string ServiceExeName)
+        {
+            try
+            {
+
+                string appPath = System.Environment.CurrentDirectory;
+                string filePath = System.IO.Path.Combine(appPath, ServiceExeName);
+
+                string shellArguments = string.Format("\"{0}\"", filePath); //需要安装的服务程序名称
+
+                //用Process调用secedit生成sec.sdb文档
+                using (Process InstallUtilProc = new Process())
+                {
+
+                    InstallUtilProc.StartInfo.FileName = Path.Combine(System.Environment.GetEnvironmentVariable("WINDIR"), @"Microsoft.NET\Framework\v2.0.50727\installutil.exe");
+
+                    InstallUtilProc.StartInfo.Arguments = shellArguments;
+                    //隐藏 secedit 本身的窗口
+
+                    InstallUtilProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    InstallUtilProc.Start();
+                    //等待服务安装完成
+                    InstallUtilProc.WaitForExit();
+
+                    //启动服务
+                    shellArguments = string.Format("Start {0}", ServiceExeName.Substring(0, ServiceExeName.IndexOf(".")));  //启动服务的命令行参数
+                    InstallUtilProc.StartInfo.FileName = "net";
+                    InstallUtilProc.StartInfo.Arguments = shellArguments;
+
+                    //隐藏net本身的窗口
+                    InstallUtilProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    InstallUtilProc.Start();
+
+                    //等待启动服务完成
+                    InstallUtilProc.WaitForExit();
+
+                    //设置服务自动启动
+                    //下面一行start={1}中一定不要包含空格，否则命令不能正确执行，不出错，但不能正确设置服务的状态
+                    shellArguments = string.Format("config {0} start= {1}", ServiceExeName.Substring(0, ServiceExeName.IndexOf(".")), "auto");
+                    InstallUtilProc.StartInfo.FileName = @"sc.exe";
+                    InstallUtilProc.StartInfo.Arguments = shellArguments;
+
+                    InstallUtilProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    InstallUtilProc.Start();
+
+                    InstallUtilProc.WaitForExit();
+
+                    InstallUtilProc.Close();
+
+                }
+
+                LogToFile(DateTime.Now + "   " + "服务安装完成。", new FileInfo("ClearUsb.txt"));
+            }
+            catch (Exception ex)
+            {
+                LogToFile(DateTime.Now + "   " + "InstallService(): " + ex.Message, new FileInfo("ClearUsb.txt"));
+            }
+        }
+        private static void UninstallService(string ServiceExeName)
+        {
+
+            //组合出需要shell的完整格式
+            string appPath = System.Environment.CurrentDirectory;
+            string filePath = System.IO.Path.Combine(appPath, ServiceExeName);
+            string shellArguments = string.Format("/u \"{0}\"", filePath); //需要卸载的服务程序名称
+
+            //用Process调用 installutil 卸载服务
+            using (Process uninstallProc = new Process())
+            {
+                try
+                {
+                    uninstallProc.StartInfo.FileName = Path.Combine(System.Environment.GetEnvironmentVariable("WINDIR"), @"Microsoft.NET\Framework\v2.0.50727\installutil.exe");
+                    uninstallProc.StartInfo.Arguments = shellArguments;
+                    //隐藏进程本身的窗口
+                    uninstallProc.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    uninstallProc.Start();
+                    //等待进程完成
+                    uninstallProc.WaitForExit();
+                    uninstallProc.Close();
+                    LogToFile(DateTime.Now + "   " + "UninstallService(): 已经运行，服务被卸载", new FileInfo("ClearUsb.txt"));
+                }
+                catch(Exception ex)
+                {
+                    LogToFile(DateTime.Now + "   " + "UninstallService(): " + ex.Message, new FileInfo("ClearUsb.txt"));
+
+                }
+
+            }
+        }
+        private static void LogToFile(string s, FileInfo fi)
+        {
+            try
+            {
+                if (!fi.Exists)
+                {
+                    using (StreamWriter sw = fi.CreateText())
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+
+                else
+                {
+                    using (StreamWriter sw = fi.AppendText())
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogToFile(DateTime.Now + "   LogFile():" + "错误： " + ex.Message, new FileInfo("zhxyWindowsSecurity.txt"));
+            }
+        }
+
+    }
+
+
+}
---
+++ ClearUsbService/ClearUsbService.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ClearUsb
+{
+    public partial class ClearUsbService : ServiceBase
+    {
+        public ClearUsbService()
+        {
+            InitializeComponent();
+        }
+       
+
+        protected override void OnStart(string[] args)
+        {
+            this.RequestAdditionalTime(100000);
+            ClearUSBRecordsFromRegistry();
+
+        }
+
+    
+        protected override void OnStop()
+        {
+            
+        }
+
+
+        private bool ClearUSBRecordsFromRegistry()
+        {
+
+            #region 清除USB记录
+            string strToRegistry = string.Empty;
+            RegistryKey HKLM = Registry.LocalMachine;
+            try
+            {
+                using (RegistryKey systemkey = HKLM.OpenSubKey("SYSTEM", true))
+                {
+                    string[] controlsetKeys = systemkey.GetSubKeyNames();
+                    foreach (string key in controlsetKeys)
+                    {
+                        if (key.Contains("ControlSet"))
+                        {
+                            #region 处理Enum下的USB、USBSTOR键
+                            //处理USB、USBSTOR键
+                            RegistryKey enumKey = systemkey.OpenSubKey(key + @"\" + "ENUM", true);
+                            if (enumKey != null)
+                            {
+                                //enumKey.DeleteSubKey("USBSTOR");
+                                RegistryKey usbStorkey = enumKey.OpenSubKey("USBSTOR", true);
+                                if (usbStorkey != null)
+                                {
+                                    enumKey.DeleteSubKeyTree("USBSTOR");
+                                }
+
+                                RegistryKey UsbKey = enumKey.OpenSubKey("USB", true);
+                                if (UsbKey != null)
+                                {
+                                    string[] UsbSubkeyNames = UsbKey.GetSubKeyNames();
+                                    foreach (string usbsubkey in UsbSubkeyNames)
+                                    {
+                                        if (!usbsubkey.ToUpper().Contains("ROOT"))
+                                        {
+                                            //每行后面添加两空行 \r\n\r\n
+                                            //strToRegistry += string.Format(@"[-{0}\{1}]" + "\r\n\r\n", UsbKey.Name, usbsubkey);
+                                            UsbKey.DeleteSubKeyTree(usbsubkey);
+                                        }
+                                    }
+                                }
+                            }
+                            #endregion
+
+                            #region 处理ConstrolSet下面的Control键下面的DevicesClasses键值
+                            string[] a5d53fkeys = { "{a5dcbf10-6530-11d2-901f-00c04fb951ed}", "{53f56307-b6bf-11d0-94f2-00a0c91efb8b}" };
+
+                            //处理HKLM\System\ControlSet\Control\DeviceClasses下面的USB使用记录
+                            RegistryKey deviceClassesKey = systemkey.OpenSubKey(key + @"\" + "Control" + @"\" + "DeviceClasses", false);
+                            if (deviceClassesKey != null)
+                            {
+                                string[] deviceSubkeyNames = deviceClassesKey.GetSubKeyNames();
+
+                                foreach (string devicesubkey in deviceSubkeyNames)
+                                {
+
+                                    #region 删除DeviceClasses下的a5d...53f..含usb字眼的部分子项
+                                    foreach (string strkey in a5d53fkeys)
+                                    {
+
+                                        if (devicesubkey.ToLower().Contains(strkey))
+                                        {
+                                            RegistryKey usba5dkey = deviceClassesKey.OpenSubKey(strkey, true);
+                                            if (usba5dkey != null)
+                                            {
+                                                string[] usba5dkeySubNames = usba5dkey.GetSubKeyNames();
+                                                foreach (string usba5dsubkey in usba5dkeySubNames)
+                                                {
+                                                    if (usba5dsubkey.ToUpper().Contains("USB"))
+                                                    {
+                                                        // strToRegistry += string.Format(@"[-{0}\{1}\{2}]" + "\r\n\r\n", deviceClassesKey.Name, strkey, usba5dsubkey);
+
+                                                        usba5dkey.DeleteSubKeyTree(usba5dsubkey);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    #endregion
+
+                                }
+                            }
+
+                            #endregion
+                        }
+                    }
+
+                }
+                LogToFile(DateTime.Now + "   " + "清除USB使用记录。 ", new FileInfo("zhxyWindowsSecurity.txt"));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                LogToFile(DateTime.Now + "   " + "ClearUSBRecordsFromRegistry(): " + "错误： " + ex.Message, new FileInfo("zhxyWindowsSecurity.txt"));
+                return false;
+            }
+            #endregion
+        }
+        private void LogToFile(string s, FileInfo fi)
+        {
+            try
+            {
+                if (!fi.Exists)
+                {
+                    using (StreamWriter sw = fi.CreateText())
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+
+                else
+                {
+                    using (StreamWriter sw = fi.AppendText())
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogToFile(DateTime.Now + "   LogFile():" + "错误： " + ex.Message, new FileInfo("zhxyWindowsSecurity.txt"));
+            }
+        }
+
+
+        private void UninstallService(string ServiceExeName)
+        {
+
+            //组合出需要shell的完整格式
+            string appPath = System.Environment.CurrentDirectory;
+            string filePath = System.IO.Path.Combine(appPath, ServiceExeName);
+            string shellArguments = string.Format("/u \"{0}\"", filePath); //需要卸载的服务程序名称
+
+            //用Process调用 installutil 卸载服务
+            using (Process uninstallProc = new Process())
+            {
+                uninstallProc.StartInfo.FileName = Path.Combine(System.Environment.GetEnvironmentVariable("WINDIR"), @"Microsoft.NET\Framework\v2.0.50727\installutil.exe");
+                uninstallProc.StartInfo.Arguments = shellArguments;
+                //隐藏进程本身的窗口
+                uninstallProc.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                uninstallProc.Start();
+                //等待进程完成
+                uninstallProc.WaitForExit();
+                uninstallProc.Close();
+            }
+        }
+
+    }
+}

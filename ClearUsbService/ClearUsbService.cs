81587592bdedddef129ceb9fed06d53b251b59e5
czwind <82054038@qq.com>
2019-05-24 15:25:56 +0800

    '更新了键值，只修改currentControlSet'

--- ClearUsbService/ClearUsbService.cs
+++ ClearUsbService/ClearUsbService.cs
@@ -42,83 +42,81 @@ namespace ClearUsb
             {
                 using (RegistryKey systemkey = HKLM.OpenSubKey("SYSTEM", true))
                 {
-                    string[] controlsetKeys = systemkey.GetSubKeyNames();
-                    foreach (string key in controlsetKeys)
-                    {
-                        if (key.Contains("ControlSet"))
-                        {
-                            #region 处理Enum下的USB、USBSTOR键
-                            //处理USB、USBSTOR键
-                            RegistryKey enumKey = systemkey.OpenSubKey(key + @"\" + "ENUM", true);
-                            if (enumKey != null)
-                            {
-                                //enumKey.DeleteSubKey("USBSTOR");
-                                RegistryKey usbStorkey = enumKey.OpenSubKey("USBSTOR", true);
-                                if (usbStorkey != null)
-                                {
-                                    enumKey.DeleteSubKeyTree("USBSTOR");
-                                }
+                    string key = "CurrentControlSet";
 
-                                RegistryKey UsbKey = enumKey.OpenSubKey("USB", true);
-                                if (UsbKey != null)
+                    #region 处理Enum下的USB、USBSTOR键
+                    //处理USB、USBSTOR键
+
+                    RegistryKey enumKey = systemkey.OpenSubKey(key + @"\" + "ENUM", true);
+                        if (enumKey != null)
+                        {
+                            //enumKey.DeleteSubKey("USBSTOR");
+                            RegistryKey usbStorkey = enumKey.OpenSubKey("USBSTOR", true);
+                            if (usbStorkey != null)
+                            {
+                                enumKey.DeleteSubKeyTree("USBSTOR");
+                            }
+
+                            RegistryKey UsbKey = enumKey.OpenSubKey("USB", true);
+                            if (UsbKey != null)
+                            {
+                                string[] UsbSubkeyNames = UsbKey.GetSubKeyNames();
+                                foreach (string usbsubkey in UsbSubkeyNames)
                                 {
-                                    string[] UsbSubkeyNames = UsbKey.GetSubKeyNames();
-                                    foreach (string usbsubkey in UsbSubkeyNames)
+                                    if (!usbsubkey.ToUpper().Contains("ROOT"))
                                     {
-                                        if (!usbsubkey.ToUpper().Contains("ROOT"))
-                                        {
-                                            //每行后面添加两空行 \r\n\r\n
-                                            //strToRegistry += string.Format(@"[-{0}\{1}]" + "\r\n\r\n", UsbKey.Name, usbsubkey);
-                                            UsbKey.DeleteSubKeyTree(usbsubkey);
-                                        }
+                                        //每行后面添加两空行 \r\n\r\n
+                                        //strToRegistry += string.Format(@"[-{0}\{1}]" + "\r\n\r\n", UsbKey.Name, usbsubkey);
+                                        UsbKey.DeleteSubKeyTree(usbsubkey);
                                     }
                                 }
                             }
-                            #endregion
+                        }
+                    #endregion
 
-                            #region 处理ConstrolSet下面的Control键下面的DevicesClasses键值
-                            string[] a5d53fkeys = { "{a5dcbf10-6530-11d2-901f-00c04fb951ed}", "{53f56307-b6bf-11d0-94f2-00a0c91efb8b}" };
+                    #region 处理ConstrolSet下面的Control键下面的DevicesClasses键值
+                    string[] a5d53fkeys = { "{2da1fe75-aab3-4d2c-acdf-39088cada665}","{325ddf96-938c-11d3-9e34-0080c82727f4}", "{a5dcbf10-6530-11d2-901f-00c04fb951ed}", "{53f56307-b6bf-11d0-94f2-00a0c91efb8b}",};
 
-                            //处理HKLM\System\ControlSet\Control\DeviceClasses下面的USB使用记录
-                            RegistryKey deviceClassesKey = systemkey.OpenSubKey(key + @"\" + "Control" + @"\" + "DeviceClasses", false);
-                            if (deviceClassesKey != null)
+                    //处理HKLM\System\ControlSet\Control\DeviceClasses下面的USB使用记录
+                    RegistryKey deviceClassesKey = systemkey.OpenSubKey(key + @"\" + "Control" + @"\" + "DeviceClasses", false);
+                    if (deviceClassesKey != null)
+                    {
+                        string[] deviceSubkeyNames = deviceClassesKey.GetSubKeyNames();
+
+                        foreach (string devicesubkey in deviceSubkeyNames)
+                        {
+
+                            #region 删除DeviceClasses下的a5d...53f..含usb字眼的部分子项
+                            foreach (string strkey in a5d53fkeys)
                             {
-                                string[] deviceSubkeyNames = deviceClassesKey.GetSubKeyNames();
 
-                                foreach (string devicesubkey in deviceSubkeyNames)
+                                if (devicesubkey.ToLower().Contains(strkey))
                                 {
-
-                                    #region 删除DeviceClasses下的a5d...53f..含usb字眼的部分子项
-                                    foreach (string strkey in a5d53fkeys)
+                                    RegistryKey usba5dkey = deviceClassesKey.OpenSubKey(strkey, true);
+                                    if (usba5dkey != null)
                                     {
-
-                                        if (devicesubkey.ToLower().Contains(strkey))
+                                        string[] usba5dkeySubNames = usba5dkey.GetSubKeyNames();
+                                        foreach (string usba5dsubkey in usba5dkeySubNames)
                                         {
-                                            RegistryKey usba5dkey = deviceClassesKey.OpenSubKey(strkey, true);
-                                            if (usba5dkey != null)
+                                            if (usba5dsubkey.ToUpper().Contains("USB"))
                                             {
-                                                string[] usba5dkeySubNames = usba5dkey.GetSubKeyNames();
-                                                foreach (string usba5dsubkey in usba5dkeySubNames)
-                                                {
-                                                    if (usba5dsubkey.ToUpper().Contains("USB"))
-                                                    {
-                                                        // strToRegistry += string.Format(@"[-{0}\{1}\{2}]" + "\r\n\r\n", deviceClassesKey.Name, strkey, usba5dsubkey);
+                                                // strToRegistry += string.Format(@"[-{0}\{1}\{2}]" + "\r\n\r\n", deviceClassesKey.Name, strkey, usba5dsubkey);
 
-                                                        usba5dkey.DeleteSubKeyTree(usba5dsubkey);
-                                                    }
-                                                }
+                                                usba5dkey.DeleteSubKeyTree(usba5dsubkey);
                                             }
                                         }
                                     }
-
-                                    #endregion
-
                                 }
                             }
 
                             #endregion
+
                         }
                     }
+
+                    #endregion
+                        
+                   
 
                 }
                 LogToFile(DateTime.Now + "   " + "清除USB使用记录。 ", new FileInfo("zhxyWindowsSecurity.txt"));
